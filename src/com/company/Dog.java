fd2b4369fbb3b9989c528658a8c9158913fcfc05
Aaron Halbert <ahalbert@gmail.com>
2017-12-03 15:17:08 -0700

    first commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Dog {
+    String name;
+
+    public void bark() {
+        System.out.println(name + " says Ruff!");
+     }
+
+    public void eat() {
+        System.out.println(name + " chews loudly!");
+    }
+
+    public void chaseCat() {
+        System.out.println(name + " nips at the cat's tail!");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Dog dog1 = new Dog();
+	    dog1.bark();
+        dog1.name = "Spot";
+
+        Dog[] myDogs = new Dog[3];
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        myDogs[0].name = "Fred";
+        myDogs[1].name = "Marge";
+
+        System.out.println("last dog's name is " + myDogs[2].name);
+
+        int x = 0;
+        while (x < myDogs.length) {
+            myDogs[x].bark();
+            x++;
+        }
+    }
+}
